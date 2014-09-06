ca05742f930d7a1b6d0aa5832e03011f0dc0fd59
Tom Carrick <tom@calmdigital.com>
2014-09-06 16:39:53 +0100

    Import test site

--- knyght/urls.py
+++ knyght/urls.py
@@ -1,10 +1,9 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'knyght.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='base.html'), name='home'),
 
     url(r'^admin/', include(admin.site.urls)),
-)
+]
