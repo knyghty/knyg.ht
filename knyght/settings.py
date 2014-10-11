fbe5a8135a5a9deb57e0cedcc09bd3e7cc7a81c4
Tom Carrick <knyght@gmail.com>
2014-10-11 19:13:28 +0100

    Add manifesto

--- knyght/settings.py
+++ knyght/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'manifesto',
     'mptt',
     'pipeline',
     'pages',
@@ -113,7 +114,6 @@ PIPELINE_COMPILERS = (
 )
 
 PIPELINE_CSS = {
-
     'main': {
         'source_filenames': (
             os.path.join('less', 'normalise.less'),
